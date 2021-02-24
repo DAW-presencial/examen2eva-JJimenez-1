c17fa60fc6c1135c716b2523ce82acb37cd36515
JJimenez-1 <joselito0310@gmail.com>
2021-02-24 17:48:01 +0100

    feat(lang): Añadir lang en ingles

---
+++ resources/lang/en/Tutores.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'empresa' => "Company",
+    'tipus_document' => "type_document",
+    'dni' => "dni",
+    'nom_tutor' => "name_tutor",
+    'primer_llinatge' => 'first_surname',
+    'segon_llinatge' => 'second_surname',
+    'pais_dni' => 'country_dni',
+    'provincia' => 'provence',
+    'municipi' => 'minicipi',
+    'estat' => 'estate',
+    'telefon' => 'phone'
+];

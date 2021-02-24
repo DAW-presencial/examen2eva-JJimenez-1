0e53437aa8b2d75d6759af5dd1bbf5072a8307cd
JJimenez-1 <joselito0310@gmail.com>
2021-02-24 17:37:12 +0100

    feat(Añadir modelo Tutor): Añadir un modelo Tutor

--- app/Models/Tutor.php
+++ app/Models/Tutor.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Tutor extends Model
 {
     use HasFactory;
+    public $fillable = ['empresa', 'tipus_document', 'dni', 'nom_tutor', 'primer_llinatge', 'segon_llinatge', 'pais_dni', 'provincia', 'municipi', 'estat', 'telefon', 'email'];
+
 }

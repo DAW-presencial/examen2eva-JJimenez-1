96f375f087cb47ff4fe3efa23c56fbc97de96f6b
JJimenez-1 <joselito0310@gmail.com>
2021-02-24 16:12:38 +0100

    feat(Migracion): Creada migracion del pantallazo tutores

---
+++ app/Models/Tutor.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tutor extends Model
+{
+    use HasFactory;
+    protected $table='tutor';
+
+    protected $fillable = [
+        'empresa',
+        'tipus_document',
+        'dni',
+        'nom_tutor',
+        'primer_llinatge',
+        'segon_llinatge',
+        'pais_dni',
+        'provincia',
+        'municipi',
+        'estat',
+        'telefon',
+        'email',
+    ];
+}
---
+++ database/migrations/2021_02_24_144607_create_tutors_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTutorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tutor', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->string('empresa');
+            $table->string('tipus_document');
+            $table->string('dni')->unique();
+            $table->string('nom_tutor');
+            $table->string('primer_llinatge');
+            $table->string('segon_llinatge');
+            $table->string('pais_dni');
+            $table->timestamp('provincia');
+            $table->string('municipi');
+            $table->string('estat');
+            $table->string('telefon')->unique();
+            $table->string('email')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tutor');
+    }
+}

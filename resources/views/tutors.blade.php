b0b0d769bdc4c8257f92dcf17b9ca6640c5a1c88
JJimenez-1 <joselito0310@gmail.com>
2021-02-24 16:43:39 +0100

    feat(tutors): Pagina tutors creada

--- resources/views/tutors.blade.php
+++ resources/views/tutors.blade.php
@@ -1,12 +1,64 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+    <h1>Tutor de l'empresa</h1>
+    <h2>Tutor de l'empresa</h2>
+    <div>
+        <label>Empresa:</label>
+        <input class="form control" name="empresa">
+    </div>
+    <div>
+        <label>Tipus document:</label>
+        <input class="form control" name="nombre_empresa">
+    </div>
+    <div>
+        <label>Document identidad:</label>
+        <input class="form control" name="dni">
+    </div>
+    <div>
+        <label>Nom:</label>
+        <input class="form control" name="nom_tutor">
+    </div>
+    <div>
+        <label>Primer llinatge:</label>
+        <input class="form control" name="primer_llinatge">
+    </div>
+    <div>
+        <label>Segon Llinatge:</label>
+        <input class="form control" name="segon_llinatge">
+    </div>
+    <div>
+        <label>Pais document identidad</label>
+        <input class="form control" name="pais_dni">
+    </div>
+    <div>
+        <label>Provincia</label>
+        <input class="form control" name="provincia">
+    </div>
+    <div>
+        <label>Municpi</label>
+        <input class="form control" name="municipi">
+    </div>
+    <div>
+        <label>Estat</label>
+        <input class="form control" name="estat">
+    </div>
+    <div>
+        <label>Telefon</label>
+        <input class="form control" name="telefon">
+    </div>
+    <div>
+        <label>Email</label>
+        <input class="form control" name="email">
+    </div>
 </body>
+
 </html>

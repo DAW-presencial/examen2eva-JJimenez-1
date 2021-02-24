73eac26d43fe13777c15ff0898c362f0ad0c38b8
JJimenez-1 <joselito0310@gmail.com>
2021-02-24 17:52:09 +0100

    feat(Lenguaje): No posible iniciar lenguaje en español

--- resources/views/tutors.blade.php
+++ resources/views/tutors.blade.php
@@ -15,43 +15,43 @@
     <h2>Tutor de l'empresa</h2>
     <form>
         <div class="form-group">
-            <label for="empresa">@lang('formTutores.empresa')</label>
+            <label for="empresa">Empresa</label>
             <input type="text" class="form-control" id="empresa" placeholder="Enter empresa" value="{{ old('empresa') }}">
         </div>
         <div class="form-group">
-            <label for="tipus_document">@lang('formTutores.tipus_document')</label>
+            <label for="tipus_document">Tipus document </label>
             <input type="text" class="form-control" id="tipus_document" placeholder="tipus_document" value="{{ old('tipus_docuemnt') }}">
         </div>
         <div class="form-group">
-            <label for="dni">@lang('formTutores.dni')</label>
+            <label for="dni">DNI</label>
             <input type="text" class="form-control" id="dni" placeholder="dni" value="{{ old('dni') }}">
         </div>
         <div class="form-group">
-            <label for="nom_tutor">@lang('formTutores.nom_tutor')</label>
+            <label for="nom_tutor">Nom</label>
             <input type="text" class="form-control" id="nom_tutor" placeholder="nom_tutor" value="{{ old('nom_tutor') }}">
         </div>
         <div class="form-group">
-            <label for="primer_llinatge">@lang('formTutores.primer_llinatge')</label>
+            <label for="primer_llinatge">primer_llinatge</label>
             <input type="text" class="form-control" id="primer_llinatge" placeholder="primer_llinatge" value="{{ old('primer_llinatge') }}">
         </div>
         <div class="form-group">
-            <label for="segon_llinatge">@lang('formTutores.segon_llinatge')</label>
+            <label for="segon_llinatge">segon_llinatge</label>
             <input type="text" class="form-control" id="segon_llinatge" placeholder="segon_llinatge" value="{{ old('segon_llinatge') }}">
         </div>
         <div class="form-group">
-            <label for="pais_dni">@lang('formTutores.pais_dni')</label>
+            <label for="pais_dni">pais_dni</label>
             <input type="text" class="form-control" id="pais_dni" placeholder="pais_dni" value="{{ old('pais_dni') }}">
         </div>
         <div class="form-group">
-            <label for="provincia">@lang('formTutores.provincia')</label>
+            <label for="provincia">provincia</label>
             <input type="text" class="form-control" id="provincia" placeholder="provincia" value="{{ old('provincia') }}">
         </div>
         <div class="form-group">
-            <label for="municipi">@lang('formTutores.municipi')</label>
+            <label for="municipi">municipi</label>
             <input type="text" class="form-control" id="municipi" placeholder="municipi" value="{{ old('municipi') }}">
         </div>
         <div class="form-group">
-            <label for="estat">@lang('formTutores.estat')</label>
+            <label for="estat">estat</label>
             <select class="form-control" id="estat">
             <option value="activo" {{ (old('estado') == "activo") ? "selected" :""}}>Activo</option>
             <option value="activo">Activo</option>
@@ -59,11 +59,11 @@
             </select>
         </div>
         <div class="form-group">
-            <label for="telefon">@lang('formTutores.telefon')</label>
+            <label for="telefon">telefon</label>
             <input type="text" class="form-control" id="telefon" placeholder="telefon" value="{{ old('telefon') }}">
         </div>
         <div class="form-group">
-            <label for="provincia">@lang('formTutores.provincia')</label>
+            <label for="provincia">provincia</label>
             <input type="text" class="form-control" id="provincia" placeholder="provincia" value="{{ old('provincia') }}">
         </div>
         <button type="submit" class="btn btn-primary">Desa</button>

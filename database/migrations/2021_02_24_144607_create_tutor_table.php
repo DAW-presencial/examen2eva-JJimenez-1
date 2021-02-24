c4c4682ff33fc94f73698e765736b1a8c7f9fbbb
JJimenez-1 <joselito0310@gmail.com>
2021-02-24 17:01:05 +0100

    fix(Tutor table): Turo table bug

--- database/migrations/2021_02_24_144607_create_tutor_table.php
+++ database/migrations/2021_02_24_144607_create_tutor_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTutorsTable extends Migration
+class CreateTutorTable extends Migration
 {
     /**
      * Run the migrations.
